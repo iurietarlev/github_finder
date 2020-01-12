010d6870272039f0a076e8ac449318ae2c68e5e3
osk849 <iura.tarlev@gmail.com>
2020-01-12 20:40:43 +0000

    final working version (used context API for state management)

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //import Repos from './Repos';
 
-export const RepoItem = ({ repo }) => {
+const RepoItem = ({ repo }) => {
   return (
     <div className="card">
       <h3>
